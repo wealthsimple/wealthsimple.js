59aa87ed095f4ab4cf65ee8c7f7ad18761fb4c30
Peter Graham <peterghm@gmail.com>
2018-02-01 00:24:19 -0500

    better formatting

--- samples/authenticated-request.js
+++ samples/authenticated-request.js
@@ -8,22 +8,22 @@ const util = require('util');
 const wealthsimple = Wealthsimple.ApiClient.instance;
 
 var postBody = {
- "client_id": "58a99e4862a1b246a7745523ca230e61dd7feff351056fcb22c73a5d7a2fcd69",
- "grant_type": "password",
- "username": process.env.OAUTH_USERNAME,
- "password": process.env.OAUTH_PASSWORD,
- "scope": "read"
+ client_id: '58a99e4862a1b246a7745523ca230e61dd7feff351056fcb22c73a5d7a2fcd69',
+ grant_type: 'password',
+ username: process.env.OAUTH_USERNAME,
+ password: process.env.OAUTH_PASSWORD,
+ scope: 'read',
 };
 var authNames = ['Bearer'];
 var contentTypes = ['application/json'];
 var accepts = ['application/json'];
 var returnType = 'String';
 
-wealthsimple.callApi("/oauth/token", "POST", {}, {}, {}, {}, {},
+wealthsimple.callApi('/oauth/token', 'POST', {}, {}, {}, {}, {},
   postBody, authNames, contentTypes, accepts, returnType, (error, data, response) => {
 
   if (error) {
-    console.log("Auth error: ", error);
+    console.log('Auth error: ', error);
   } else {
     const responseJSON = JSON.parse(data);
     let Bearer = wealthsimple.authentications['Bearer'];
@@ -31,8 +31,8 @@ wealthsimple.callApi("/oauth/token", "POST", {}, {}, {}, {}, {},
     Bearer.apiKeyPrefix = 'Bearer';
 
     const usersApi = new Wealthsimple.UsersApi();
-    
-    usersApi.getUser("user-221_1ut5ujy", (error, data, response) => {
+
+    usersApi.getUser('user-221_1ut5ujy', (error, data, response) => {
       if (error) {
         console.error(error);
       } else {
