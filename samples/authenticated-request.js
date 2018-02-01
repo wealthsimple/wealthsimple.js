0fc0b8da7aeb87f5cd144e7adee959147c12e9ea
Peter Graham <peterghm@gmail.com>
2018-02-01 00:06:40 -0500

    fully working getUser example

--- samples/authenticated-request.js
+++ samples/authenticated-request.js
@@ -1,3 +1,6 @@
+// This example shows how to get authenticated user data.
+// You must set OAUTH_USERNAME + OAUTH_PASSWORD in `.env` corresponding to user-221_1ut5ujy 
+
 require('dotenv').config();
 const Wealthsimple = require('../src/index');
 const util = require('util');
@@ -17,27 +20,26 @@ var accepts = ['application/json'];
 var returnType = 'String';
 
 const authCallback = (error, data, response) => {
-  console.log("AUTH")
   if (error) {
-    console.log("ERROR", error);
+    console.log("Auth error: ", error);
   } else {
-    console.log("DATA", data);
+    const responseJSON = JSON.parse(data);
+    let Bearer = defaultClient.authentications['Bearer'];
+    Bearer.apiKey = responseJSON.access_token;
+    Bearer.apiKeyPrefix = 'Bearer';
+
+    const apiInstance = new Wealthsimple.UsersApi();
+
+    const getUserCallback = (error, data, response) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('API called successfully. Returned data: ');
+        console.log(util.inspect(data, false, null));
+      }
+    };
+    apiInstance.getUser("user-221_1ut5ujy", getUserCallback);
   }
 };
 
 defaultClient.callApi("/oauth/token", "POST", {}, {}, {}, {}, {}, postBody, authNames, contentTypes, accepts, returnType, authCallback);
-
-// let Bearer = defaultClient.authentications['Bearer'];
-// Bearer.apiKey = 'YOUR API KEY';
-// Bearer.apiKeyPrefix = 'Bearer';
-//
-// const apiInstance = new Wealthsimple.UsersApi();
-//
-// const callback = (error, data, response) => {
-//   if (error) {
-//     console.error(error);
-//   } else {
-//     console.log('API called successfully. Returned data: ' + data);
-//   }
-// };
-// apiInstance.getUser("user-221_1ut5ujy", callback);
