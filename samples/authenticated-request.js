ae57e0eb7350085a5105348fb06f966ed1fe1801
Peter Graham <peterghm@gmail.com>
2018-02-01 00:23:26 -0500

    better readability

--- samples/authenticated-request.js
+++ samples/authenticated-request.js
@@ -1,11 +1,11 @@
 // This example shows how to get authenticated user data.
-// You must set OAUTH_USERNAME + OAUTH_PASSWORD in `.env` corresponding to user-221_1ut5ujy 
+// You must set OAUTH_USERNAME + OAUTH_PASSWORD in `.env` corresponding to user-221_1ut5ujy
 
 require('dotenv').config();
 const Wealthsimple = require('../src/index');
 const util = require('util');
 
-const defaultClient = Wealthsimple.ApiClient.instance;
+const wealthsimple = Wealthsimple.ApiClient.instance;
 
 var postBody = {
  "client_id": "58a99e4862a1b246a7745523ca230e61dd7feff351056fcb22c73a5d7a2fcd69",
@@ -19,27 +19,26 @@ var contentTypes = ['application/json'];
 var accepts = ['application/json'];
 var returnType = 'String';
 
-const authCallback = (error, data, response) => {
+wealthsimple.callApi("/oauth/token", "POST", {}, {}, {}, {}, {},
+  postBody, authNames, contentTypes, accepts, returnType, (error, data, response) => {
+
   if (error) {
     console.log("Auth error: ", error);
   } else {
     const responseJSON = JSON.parse(data);
-    let Bearer = defaultClient.authentications['Bearer'];
+    let Bearer = wealthsimple.authentications['Bearer'];
     Bearer.apiKey = responseJSON.access_token;
     Bearer.apiKeyPrefix = 'Bearer';
 
-    const apiInstance = new Wealthsimple.UsersApi();
-
-    const getUserCallback = (error, data, response) => {
+    const usersApi = new Wealthsimple.UsersApi();
+    
+    usersApi.getUser("user-221_1ut5ujy", (error, data, response) => {
       if (error) {
         console.error(error);
       } else {
         console.log('API called successfully. Returned data: ');
         console.log(util.inspect(data, false, null));
       }
-    };
-    apiInstance.getUser("user-221_1ut5ujy", getUserCallback);
+    });
   }
-};
-
-defaultClient.callApi("/oauth/token", "POST", {}, {}, {}, {}, {}, postBody, authNames, contentTypes, accepts, returnType, authCallback);
+});
--- samples/healthcheck.js
+++ samples/healthcheck.js
@@ -1,14 +1,13 @@
 const Wealthsimple = require('../src/index');
 const util = require('util');
 
-const apiInstance = new Wealthsimple.HealthcheckApi();
+const healthcheckApi = new Wealthsimple.HealthcheckApi();
 
-const callback = (error, data, response) => {
+healthcheckApi.healthcheck((error, data, response) => {
   if (error) {
     console.error(error);
   } else {
     console.log('API called successfully. Returned data: ');
     console.log(util.inspect(data, false, null));
   }
-};
-apiInstance.healthcheck(callback);
+});
