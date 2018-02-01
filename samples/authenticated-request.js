81147f1e893cd30484aed4e973a6703fff31ceb3
Peter Graham <peterghm@gmail.com>
2018-02-01 00:28:52 -0500

    rename confusing env variables

--- samples/authenticated-request.js
+++ samples/authenticated-request.js
@@ -1,5 +1,5 @@
 // This example shows how to get authenticated user data.
-// You must set OAUTH_USERNAME + OAUTH_PASSWORD in `.env` corresponding to user-221_1ut5ujy
+// You must set EMAIL + PASSWORD in `.env` corresponding to user-221_1ut5ujy on staging.
 
 require('dotenv').config();
 const Wealthsimple = require('../src/index');
@@ -10,8 +10,8 @@ const wealthsimple = Wealthsimple.ApiClient.instance;
 var postBody = {
   client_id: '58a99e4862a1b246a7745523ca230e61dd7feff351056fcb22c73a5d7a2fcd69',
   grant_type: 'password',
-  username: process.env.OAUTH_USERNAME,
-  password: process.env.OAUTH_PASSWORD,
+  username: process.env.EMAIL,
+  password: process.env.PASSWORD,
   scope: 'read',
 };
 var authNames = ['Bearer'];
