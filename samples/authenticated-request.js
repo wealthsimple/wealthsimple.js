c0b67ffa711b4e8f2fb9a37bfca9c5e4cac8268c
Peter Graham <peterghm@gmail.com>
2018-02-01 01:05:19 -0500

    simpler auth handling

--- samples/authenticated-request.js
+++ samples/authenticated-request.js
@@ -7,32 +7,25 @@ const util = require('util');
 
 const wealthsimple = Wealthsimple.ApiClient.instance;
 
-const postBody = {
-  client_id: '58a99e4862a1b246a7745523ca230e61dd7feff351056fcb22c73a5d7a2fcd69',
-  grant_type: 'password',
+wealthsimple.auth({
+  clientId: '58a99e4862a1b246a7745523ca230e61dd7feff351056fcb22c73a5d7a2fcd69',
+  grantType: 'password',
   username: process.env.EMAIL,
   password: process.env.PASSWORD,
   scope: 'read write',
-};
-const returnType = 'Object';
-
-wealthsimple.callApi('/oauth/token', 'POST', {}, {}, {}, {}, {},
-  postBody, [], [], [], returnType, (error, data, response) => {
-
-  if (error) {
-    console.log('Auth error: ', error);
-  } else {
-    wealthsimple.authentications['Bearer']['apiKey'] = data.access_token;
-
-    const usersApi = new Wealthsimple.UsersApi();
-
-    usersApi.getUser('user-221_1ut5ujy', (error, data, response) => {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log('API called successfully. Returned data: ');
-        console.log(util.inspect(data, false, null));
-      }
-    });
-  }
+  callback: (error, data, response) => {
+    if (error) {
+      console.log('Auth error: ', error);
+    } else {
+      const usersApi = new Wealthsimple.UsersApi();
+      usersApi.getUser('user-221_1ut5ujy', (error, data, response) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log('API called successfully. Returned data: ');
+          console.log(util.inspect(data, false, null));
+        }
+      });
+    }
+  },
 });
