944ff2f1da48801fb983d5bfb237ea34e424581f
Peter Graham <peterghm@gmail.com>
2018-02-01 00:01:58 -0500

    get auth working

--- samples/authenticated-request.js
+++ samples/authenticated-request.js
@@ -4,17 +4,40 @@ const util = require('util');
 
 const defaultClient = Wealthsimple.ApiClient.instance;
 
-let Bearer = defaultClient.authentications['Bearer'];
-Bearer.apiKey = 'YOUR API KEY';
-Bearer.apiKeyPrefix = 'Bearer';
+var postBody = {
+ "client_id": "58a99e4862a1b246a7745523ca230e61dd7feff351056fcb22c73a5d7a2fcd69",
+ "grant_type": "password",
+ "username": process.env.OAUTH_USERNAME,
+ "password": process.env.OAUTH_PASSWORD,
+ "scope": "read"
+};
+var authNames = ['Bearer'];
+var contentTypes = ['application/json'];
+var accepts = ['application/json'];
+var returnType = 'String';
 
-const apiInstance = new Wealthsimple.UsersApi();
-
-const callback = (error, data, response) => {
+const authCallback = (error, data, response) => {
+  console.log("AUTH")
   if (error) {
-    console.error(error);
+    console.log("ERROR", error);
   } else {
-    console.log('API called successfully. Returned data: ' + data);
+    console.log("DATA", data);
   }
 };
-apiInstance.getUser("user-221_1ut5ujy", callback);
+
+defaultClient.callApi("/oauth/token", "POST", {}, {}, {}, {}, {}, postBody, authNames, contentTypes, accepts, returnType, authCallback);
+
+// let Bearer = defaultClient.authentications['Bearer'];
+// Bearer.apiKey = 'YOUR API KEY';
+// Bearer.apiKeyPrefix = 'Bearer';
+//
+// const apiInstance = new Wealthsimple.UsersApi();
+//
+// const callback = (error, data, response) => {
+//   if (error) {
+//     console.error(error);
+//   } else {
+//     console.log('API called successfully. Returned data: ' + data);
+//   }
+// };
+// apiInstance.getUser("user-221_1ut5ujy", callback);
