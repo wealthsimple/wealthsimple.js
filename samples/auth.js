a61936ba77693f3eae33eea0f858a9e210dc2b1a
Peter Graham <peterghm@gmail.com>
2018-02-07 19:14:00 -0500

    in progress

---
+++ samples/auth.js
@@ -0,0 +1,16 @@
+// This example shows how to get authenticated user data.
+// You must set EMAIL + PASSWORD in `.env` corresponding to a valid staging user.
+
+require('dotenv').config();
+const Wealthsimple = require('../src/index');
+const util = require('util');
+
+const wealthsimple = new Wealthsimple({
+  env: 'sandbox',
+  clientId: '58a99e4862a1b246a7745523ca230e61dd7feff351056fcb22c73a5d7a2fcd69',
+});
+
+wealthsimple.login(process.env.EMAIL, process.env.PASSWORD)
+  .then(res => res.json())
+  .catch(error => console.error('Error:', error))
+  .then(response => console.log('Success:', response));
--- src/index.js
+++ src/index.js
@@ -1,4 +1,47 @@
+'use strict';
 
+require('es6-promise').polyfill();
+
+class Wealthsimple {
+  constructor(config) {
+    this.clientId = config.clientId;
+    this.accessToken = config.accessToken;
+    this.refreshToken = config.refreshToken;
+    this.env = config.env;
+    this.apiVersion = config.apiVersion || 'v1';
+    if (config.requestAdapter) {
+      this.fetch = config.requestAdapter;
+    } else {
+      require('isomorphic-fetch');
+      this.fetch = fetch;
+    }
   }
 
+  urlFor(path) {
+    if (!path.startsWith('/')) {
+      path = `/${path}`;
+    }
+    return `https://api.${this.env}.wealthsimple.com/${this.apiVersion}${path}`;
+  }
 
+  login(email, password) {
+    const body = {
+      clientId: this.clientId,
+      grantType: 'password',
+      username: email,
+      password: password,
+      scope: 'read write',
+    };
+    console.log(this.urlFor('/oauth/token'), JSON.stringify(body));
+    return fetch(this.urlFor('/oauth/token'), {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: new Headers({
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      })
+    });
+  }
+};
+
+module.exports = Wealthsimple;
