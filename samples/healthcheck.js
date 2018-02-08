0015f20d8a7c937849083e413ab88392f053c835
Peter Graham <peterghm@gmail.com>
2018-02-07 20:30:18 -0500

    update healthcheck example

--- samples/healthcheck.js
+++ samples/healthcheck.js
@@ -1,13 +1,6 @@
 const Wealthsimple = require('../src/index');
-const util = require('util');
 
-const healthcheckApi = new Wealthsimple.HealthcheckApi();
+const wealthsimple = new Wealthsimple({env: 'sandbox'});
 
-healthcheckApi.healthcheck((error, data, response) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('API called successfully. Returned data: ');
-    console.log(util.inspect(data, false, null));
-  }
-});
+wealthsimple.get('/healthcheck', {auth: false})
+  .then((res) => console.log(res));
