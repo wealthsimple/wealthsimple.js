8cefe97ece46b8d8f3e5bf146bc1cf01f01cd96d
Peter Graham <peterghm@gmail.com>
2018-05-14 14:54:17 -0400

    lint

--- src/api-response.js
+++ src/api-response.js
@@ -9,18 +9,21 @@ class ApiResponse {
     if (this.headers.has('x-ratelimit-reset')) {
       return new Date(Date.parse(this.headers.get('x-ratelimit-reset')));
     }
+    return null;
   }
 
   getRateLimitLimit() {
     if (this.headers.has('x-ratelimit-limit')) {
-      return parseInt(this.headers.get('x-ratelimit-limit'));
+      return parseInt(this.headers.get('x-ratelimit-limit'), 10);
     }
+    return null;
   }
 
   getRateLimitRemaining() {
     if (this.headers.has('x-ratelimit-remaining')) {
-      return parseInt(this.headers.get('x-ratelimit-remaining'));
+      return parseInt(this.headers.get('x-ratelimit-remaining'), 10);
     }
+    return null;
   }
 }
 
