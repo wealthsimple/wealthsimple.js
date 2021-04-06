5759cc2db8bb4c2d2c4d1243c4336df8ff335eba
Alexi Garrow <agarrow@wealthsimple.com>
2021-04-06 13:25:31 -0400

    preferred otp device authentication params

--- src/constants.js
+++ src/constants.js
@@ -4,4 +4,6 @@ module.exports = {
   OTP_HEADER: 'x-wealthsimple-otp',
   OTP_CLAIM_HEADER: 'x-wealthsimple-otp-claim',
   OKTA_CLAIM_HEADER: 'x-wealthsimple-okta-claim',
+  OTP_PREFERRED_DEVICE_TYPE: 'x-wealthsimple-otp-preferred-device-type',
+  OTP_PREFERRED_DEVICE_IDENTIFIER: 'x-wealthsimple-otp-preferred-device-identifier'
 };
